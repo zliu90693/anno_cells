3c46c211bff2b7281874e0665d934cfea6c04849
zliu <zliu90693@gmail.com>
2026-06-06 10:38:08 +0800

    prepare transfer anno

--- Sheng_SA_2020_Hsal/migrate_anno_Hsal.py
+++ Sheng_SA_2020_Hsal/migrate_anno_Hsal.py
@@ -28,17 +28,36 @@ Hsal_50.var['n_cells_not_detected'] = total_cells - Hsal_50.var['n_cells_by_coun
 mask = Hsal_50.var['n_cells_not_detected'] >= 20
 num_genes = mask.sum()
 # %%
-# 开始迁移注释
+# ------------------------------------- 开始迁移注释 -------------------------------------
+
+# %%
+# 注意! Hsal原文与我产生的数据在 adata.obs_names 的格式上有差异, 需要统一!!!
+# 如果不进行格式统一, 须知不同样本间的细胞条形码有可能是重复的, 仅使用条形码无法作为分辨细胞的唯一要素!!!
+
+# %%
+# %%
 Hsal_51.obs
 # %%
 Hsal_me = sc.read_h5ad("./cluster_unannotated.h5ad")
+Hsal_me.obs["cell_key"] = Hsal_me.obs.index.str.split("-").str[0]
+Hsal_me.obs
 # %%
 Hsal_51_cluster = Hsal_51.obs[["cell_type"]]
 Hsal_51_cluster["cell_key"] = Hsal_51_cluster.index.str.split("-").str[-1]
 Hsal_51_cluster
 # %%
-mycellset = set(Hsal_me.obs.index.str.split("-").str[0])
+mycellset = set(Hsal_me.obs["cell_key"])
 cellset51 = set(Hsal_51_cluster["cell_key"])
 print(f"mycellset: {len(mycellset)}, cellset51: {len(cellset51)}")
 print(len(mycellset.intersection(cellset51)))
+# %%
+Hsal_51_cluster.to_csv("./Hsal_51.csv")
+# %%
+# Hsal_me.obs = Hsal_me.obs.merge(
+#     Hsal_51_cluster, 
+#     left_index=True, 
+#     right_on="cell_key", 
+#     how="left"
+# ).rename(columns={"cell_type": "cell_type"}).fillna({"cell_type": "Unknown"})
+
 # %%
